7fe446162484458f01e9be505bc6a0875a6c9e14
Elías Fernández <elias@playfinder.com>
2024-11-12 15:39:43 +0100

    Impersonate from user menu dropdown #6524

---
+++ src/Config/Menu/AccessImpersonationMenuItem.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace EasyCorp\Bundle\EasyAdminBundle\Config\Menu;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Option\EA;
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Menu\MenuItemInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\MenuItemDto;
+use Symfony\Contracts\Translation\TranslatableInterface;
+
+/**
+ * @see EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem::linkToExitImpersonation()
+ *
+ * @author Elías Fernández <eliasfernandez@gmail.com>
+ */
+final class AccessImpersonationMenuItem implements MenuItemInterface
+{
+    use MenuItemTrait;
+
+    public function __construct(TranslatableInterface|string $label, ?string $icon)
+    {
+        $this->dto = new MenuItemDto();
+
+        $this->dto->setType(MenuItemDto::TYPE_ACCESS_IMPERSONATION);
+        $this->dto->setLabel($label);
+        $this->dto->setIcon($icon);
+        $this->dto->setLinkUrl('test');
+    }
+}
